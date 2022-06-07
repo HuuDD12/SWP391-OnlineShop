40ac37763c4b25a10b83e20d4b685e562584e59a
anhducbvb09 <105422607+anhducbvb09@users.noreply.github.com>
2022-06-07 17:24:48 +0700

    ResetPassword

---
+++ SWP391_Online_Shop/src/java/Controller/LayPassServlet.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Entity.Account;
+import Model.loginDAO;
+import SendMail.SendMail;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ADM
+ */
+public class LayPassServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String user = request.getParameter("username");
+        String maaccout = request.getParameter("password");
+        String Email = request.getParameter("Email");
+        loginDAO dao = new loginDAO();
+        SendMail send = new SendMail();
+        Account a = dao.checkMaaccount(user, maaccout);
+        if(a==null){
+            request.setAttribute("mess", "Nhập sai tên hoặc mã");
+            request.getRequestDispatcher("ResetPassword.jsp").forward(request, response);
+        }else{
+            send.GuiMatKhau(Email, a.getUsername(), a.getPassword());
+            request.setAttribute("mess", "Mật khẩu đã được gửi đến mail");
+            request.getRequestDispatcher("page-login.jsp").forward(request, response);         
+        }
+                
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
