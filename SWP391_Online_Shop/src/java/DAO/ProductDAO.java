28c7941e78a70a4fb7c75dbd1b242fcbd7b4369f
tuyet2002 <105422769+tuyet2002@users.noreply.github.com>
2022-06-14 16:29:07 +0700

    dp

--- SWP391_Online_Shop/src/java/DAO/ProductDAO.java
+++ SWP391_Online_Shop/src/java/DAO/ProductDAO.java
@@ -513,5 +513,14 @@ public class ProductDAO extends DBcontext.DBContext {
         return list;
     }
         
-    
+    public void deleteProduct(String cid) {
+        String query = "delete from ProductImg where ProductID = ?";
+        try {
+
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, cid);
+            ps.executeUpdate();
+        } catch (Exception e) {
+
+        }
 }
