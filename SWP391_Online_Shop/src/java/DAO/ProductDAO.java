665d8dc54d2d7ab61ebcce6d00cde398df9222a4
Le Thi Van Anh <90490529+AnhLTVHE151426@users.noreply.github.com>
2022-06-16 13:54:07 +0700

    Update ProductDAO.java

--- SWP391_Online_Shop/src/java/DAO/ProductDAO.java
+++ SWP391_Online_Shop/src/java/DAO/ProductDAO.java
@@ -113,7 +113,69 @@ public class ProductDAO extends DBcontext.DBContext {
         }
         return null;
     }
+public List<Product> pagingProductBy6(int indexPage) {
+        String sql = "SELECT * FROM (SELECT p.ProductID,MIN(p.ProductName) AS ProductName,MIN(p.Description) AS Description,\n"
+                + "MIN(p.OriginalPrice) AS OriginalPrice,MIN(p.SalePrice) AS SalePrice,\n"
+                + "MIN(p.SubCategoryID) AS SubCategoryID,MIN(p.Amount) AS Amount,\n"
+                + "MIN(p.BrandID) AS BrandID,MIN(p.sell_id) AS sell_id,\n"
+                + "MIN(ProI.ProductImgURL) AS ProductImgURL FROM \n"
+                + "dbo.Product p JOIN  dbo.ProductImg ProI ON ProI.ProductID = p.ProductID GROUP BY p.ProductID ) t  "
+                + "order by t.productID OFFSET ? ROWS FETCH NEXT 6 ROWS ONLY";
+        List<Product> list = new ArrayList<>();
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, (indexPage - 1) * 6);
+            //ps.setInt(2, entry);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Product p = new Product(rs.getInt("productID"),
+                        rs.getString("ProductName"),
+                        rs.getString("Description"),
+                        rs.getDouble("OriginalPrice"),
+                        rs.getDouble("SalePrice"),
+                        rs.getInt("SubCategoryID"),
+                        rs.getInt("Amount"),
+                        rs.getInt("BrandID"),
+                        rs.getInt("sell_id"),
+                        rs.getString("ProductImgURL"));
+                list.add(p);
+            }
 
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return list;
+
+    }
+    public List<Product> getTop5Product() {
+        String sql = "select top 5 p.*,pr.ProductImgURL from Product p join ProductImg pr\n"
+                + "on p.ProductID = pr.ProductID\n"
+                + "order by p.ProductID desc";
+        List<Product> list = new ArrayList<>();
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Product p = new Product(rs.getInt("productID"),
+                        rs.getString("ProductName"),
+                        rs.getString("Description"),
+                        rs.getDouble("OriginalPrice"),
+                        rs.getDouble("SalePrice"),
+                        rs.getInt("SubCategoryID"),
+                        rs.getInt("Amount"),
+                        rs.getInt("BrandID"),
+                        rs.getInt("sell_id"),
+                        rs.getString("ProductImgURL")
+                );
+                list.add(p);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+    
     public List<Product> searchProductBySubCate(int subcate) {
         String sql = "SELECT * FROM (SELECT p.ProductID,MIN(p.ProductName) AS ProductName,MIN(p.Description) AS Description,\n"
                 + "MIN(p.OriginalPrice) AS OriginalPrice,MIN(p.SalePrice) AS SalePrice,\n"
