d05b71abe41f1ee0cbf915fcca10f4925fc1f2ab
HuuDD12 <97151335+HuuDD12@users.noreply.github.com>
2022-06-30 15:27:14 +0700

    up Banner

--- SWP391_Online_Shop/src/java/Controller/HomeControl.java
+++ SWP391_Online_Shop/src/java/Controller/HomeControl.java
@@ -6,6 +6,7 @@
 package Controller;
 
 import DAO.BrandDAO;
+import DAO.BannerDAO;
 import DAO.CategoryDAO;
 import DAO.OrderDetailDAO;
 import DAO.ProductDAO;
@@ -15,6 +16,7 @@ import Model.Category;
 import Model.OrderDetail;
 import Model.Product;
 import Model.Subcategory;
+import Model.Banner;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -53,8 +55,10 @@ public class HomeControl extends HttpServlet {
             CategoryDAO c = new CategoryDAO();
             BrandDAO b = new BrandDAO();
             OrderDetailDAO o = new OrderDetailDAO();
+            BannerDAO dao = new BannerDAO();
             
             List<Product> listTop5 = pr.getTop5Product();
+            List<Banner> listBanner = dao.getAllBanner();
             List<Category> listC = c.getAllCategory();
             List<Subcategory> listS = s.getAllSubCategory();
             //Subcategory a = s.getSubCategoryByID(id);
@@ -73,6 +77,7 @@ public class HomeControl extends HttpServlet {
                 endPage++;
             }
             List<Product> list6 = pr.pagingProductBy6(index);
+            request.setAttribute("listBanner", listBanner);
             request.setAttribute("listP", list6);
             request.setAttribute("listSell", listSell);
             request.setAttribute("listC", listC);
